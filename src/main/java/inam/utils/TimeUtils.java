1ac01171a9efc56d08b41782e51bab33404cd2bf
Inamullah Taj <inamullahtaj@gmail.com>
2017-05-29 06:11:55 +0500

    added functions to calculate and process timedifferences

--- src/main/java/inam/utils/TimeUtils.java
+++ src/main/java/inam/utils/TimeUtils.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class TimeUtils {
 
@@ -11,26 +12,69 @@ public class TimeUtils {
 		if (previousDateTime.equalsIgnoreCase("") || previousDateTime.isEmpty() || previousDateTime==null) {
 			return currentDateTime;
 		}
-		String smallestDateTime = "";
-		LocalDateTime currentDateTimeObject = parseStringAndGetFormattedTimeAsLocalDateTime(currentDateTime);
-		LocalDateTime previousDateTimeObject = parseStringAndGetFormattedTimeAsLocalDateTime(previousDateTime);
+		// get difference between currentDateTimeObject and previousDateTimeObject and return the smallest
+		double hours = getHoursDifferenceBetweenTimestamps(currentDateTime, previousDateTime);
+		if (hours < 0) {
+			return currentDateTime;
+		}
 
-		// TODO
-
-		return smallestDateTime;
+		return previousDateTime;
 	}
 
 	public static String getLargestDateTime(String currentDateTime, String previousDateTime) {
 		if (previousDateTime.equalsIgnoreCase("") || previousDateTime.isEmpty() || previousDateTime==null) {
 			return currentDateTime;
 		}
-		String largestDateTime = "";
+		// get difference between currentDateTimeObject and previousDateTimeObject and return the largest
+		double hours = getHoursDifferenceBetweenTimestamps(currentDateTime, previousDateTime);
+		if (hours > 0) {
+			return currentDateTime;
+		}
+
+		return previousDateTime;
+	}
+
+	private static double getHoursDifferenceBetweenTimestamps(String currentDateTime, String previousDateTime) {
+		// calculate total time in hours between two timestamps
+
 		LocalDateTime currentDateTimeObject = parseStringAndGetFormattedTimeAsLocalDateTime(currentDateTime);
 		LocalDateTime previousDateTimeObject = parseStringAndGetFormattedTimeAsLocalDateTime(previousDateTime);
 
-		// TODO
+		LocalDateTime fromTemp = LocalDateTime.from(previousDateTimeObject);
+		long years = fromTemp.until(currentDateTimeObject, ChronoUnit.YEARS);
+		fromTemp = fromTemp.plusYears(years);
 
-		return largestDateTime;
+		long months = fromTemp.until(currentDateTimeObject, ChronoUnit.MONTHS);
+		fromTemp = fromTemp.plusMonths(months);
+
+		long days = fromTemp.until(currentDateTimeObject, ChronoUnit.DAYS);
+		fromTemp = fromTemp.plusDays(days);
+
+		long hours = fromTemp.until(currentDateTimeObject, ChronoUnit.HOURS);
+		fromTemp = fromTemp.plusHours(hours);
+
+		long minutes = fromTemp.until(currentDateTimeObject, ChronoUnit.MINUTES);
+		fromTemp = fromTemp.plusMinutes(minutes);
+
+		long seconds = fromTemp.until(currentDateTimeObject, ChronoUnit.SECONDS);
+		fromTemp = fromTemp.plusSeconds(seconds);
+
+		long millis = fromTemp.until(currentDateTimeObject, ChronoUnit.MILLIS);
+
+		double totalHours = 0;
+
+		totalHours += years * 8760;
+		totalHours += months * 730;
+		totalHours += days * 24;
+		totalHours += hours;
+		totalHours += minutes / 60;
+
+		return totalHours;
+	}
+
+	public static double getTotalHoursBetweenTwoDateTimes(String lowestTime, String highestTime) {
+		// note: highestTime should be first argument
+		return getHoursDifferenceBetweenTimestamps(highestTime, lowestTime);
 	}
 
 	public static String parseStringAndGetFormattedTimeAsString(String time) {
